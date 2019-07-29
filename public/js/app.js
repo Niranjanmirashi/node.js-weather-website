5543aa04e91c8266ee6ce2c3c7fecf54abd52ceb
Abhijeet Mirashi <abhijeet@Abhijeets-MacBook-Pro.local>
2019-07-29 15:57:14 -0700

    init commit-test

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('From a JS file')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#message-1')
