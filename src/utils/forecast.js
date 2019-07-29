dd1c5ce0dc6b4815393c4fd1eb2228ae1353e148
Niranjan Mirashi <niranjanmirashi@gmail.com>
2019-07-29 16:57:06 -0700

    Extra forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, lon, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined , body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' and has ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined , body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' and has ' + body.currently.precipProbability + '% chance of rain. \nTemperature high : ' + body.daily.data[0].temperatureHigh + '.\n Temperature low : '+ body.daily.data[0].temperatureLow)
         }
     })
 }
